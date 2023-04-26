b307e556350ef639cabd5995f576145b48251af8
evgenii <e.galimsarov@gmail.com>
2023-04-26 14:34:52 +0500

    Using ResponseStatus Annotation

--- src/main/kotlin/guru/springframework/spring6restmvc/controller/NotFoundException.kt
+++ src/main/kotlin/guru/springframework/spring6restmvc/controller/NotFoundException.kt
@@ -1,3 +1,7 @@
 package guru.springframework.spring6restmvc.controller
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value not Found")
 class NotFoundException : RuntimeException()
